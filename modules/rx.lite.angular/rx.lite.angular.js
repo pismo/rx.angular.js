26e72b563c3d525b53adf8b7e6deb6872e0cf7b8
Daniel Paulino <danielpaulino030@gmail.com>
2016-05-16 14:29:17 -0300

    Update dist

--- modules/rx.lite.angular/rx.lite.angular.js
+++ modules/rx.lite.angular/rx.lite.angular.js
@@ -597,11 +597,11 @@ function thrower(e) {
       var $scope = this.$scope;
 
       if ($scope.$$phase || $scope.$root.$$phase) {
-        sad.setDisposable(Rx.Disposable._fixup(state(action)));
+        sad.setDisposable(Rx.Disposable._fixup(action(this, state)));
       } else {
         $scope.$apply.call(
           $scope,
-          function () { sad.setDisposable(Rx.Disposable._fixup(state(action))); }
+          function () { sad.setDisposable(Rx.Disposable._fixup(action(this, state))); }
         );
       }
     };
